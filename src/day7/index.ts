555ddaa873d27dbe04421cf91b7f7a2ac73e7e1b
Christa <christa@infi.nl>
2022-12-29 17:14:41 +0100

    Day 7

--- src/day7/index.ts
+++ src/day7/index.ts
@@ -1,18 +1,113 @@
 import { Day } from "../day";
+import { sum } from "../utils/utils";
 
-class Day7 extends Day {
+type Dir = {
+  name: string;
+  parent: Dir | null;
+  children: Dir[];
+  size: number;
+};
 
-    constructor(){
-        super(7);
+let fileSystem: Dir = {
+  name: "root",
+  parent: null,
+  children: [],
+  size: 0,
+};
+
+let currentDir: Dir = fileSystem;
+let allDirSizes: number[] = [];
+
+function parseLine(line: string) {
+  const splittedLine = line.split(" ");
+
+  switch (splittedLine[0]) {
+    case "$": {
+      switch (splittedLine[1]) {
+        case "cd": {
+          // change dir
+          switch (splittedLine[2]) {
+            case "/": {
+              break;
+            }
+            default: {
+              if (splittedLine[2].startsWith("..")) {
+                currentDir = currentDir.parent || fileSystem;
+              } else {
+                // move down
+                let dirName = splittedLine[2];
+
+                let newDir = currentDir.children.find(
+                  (dir) => dir.name === dirName
+                );
+
+                if (newDir) {
+                  currentDir = newDir;
+                }
+              }
+            }
+          }
+
+          break;
+        }
+        case "ls": {
+          break;
+        }
+      }
+      break;
     }
-
-    solveForPartOne(input: string): string {
-        return input;
+    default: {
+      if (splittedLine[0].startsWith("dir")) {
+        // add dir to children
+        let newDir: Dir = {
+          name: splittedLine[1],
+          parent: currentDir,
+          children: [],
+          size: 0,
+        };
+        currentDir.children.push(newDir);
+      } else {
+        // add file size to dir size
+        currentDir.size += parseInt(splittedLine[0]);
+      }
+      break;
     }
-
-    solveForPartTwo(input: string): string {
-        return input;
-    }
+  }
 }
 
-export default new Day7;
+function traverseTree(dir: Dir) {
+  dir.children.forEach((childDir) => {
+    traverseTree(childDir);
+  });
+
+  dir.children.forEach((childDir) => (dir.size += childDir.size));
+
+  allDirSizes.push(dir.size);
+}
+
+class Day7 extends Day {
+  constructor() {
+    super(7);
+  }
+
+  // 1206825
+  solveForPartOne(input: string): string {
+    const lines = input.split("\n");
+    lines.forEach((line) => parseLine(line));
+
+    traverseTree(fileSystem);
+
+    return sum(allDirSizes.filter((size) => size <= 100000)).toString();
+  }
+
+  // 9608311
+  solveForPartTwo(input: string): string {
+    const minSpaceToDelete = 30000000 - (70000000 - fileSystem.size);
+
+    return Math.min(
+      ...allDirSizes.filter((size) => size >= minSpaceToDelete)
+    ).toString();
+  }
+}
+
+export default new Day7();
